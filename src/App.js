fd2f6e06c717c2916ca1eb4f63bdee6bab52cdcc
ohsujinsuoh <dhtnwlstndh@naver.com>
2023-10-20 14:14:53 +0900

    HTML mark up real

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-//HTML
-//Mark up
-//<div> <p> hello world </p> </div>
-
-
-//CSS
-//꾸미기 언어
-
-
-<input/>;
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  Const [todos, setTodos] = useState([]);
+  // react way
+  const [todos, setTodos] = useState(["할일 1", "할일 2"]);
+  // state 변화하는 값
+  // React -> state가 변할때마다 화면을 다시 그린다.
+  // ["할일 1", "할일 2"];
 
   return (
+    // JSX (JS -> HTML)
     <div className="App">
       <h1>TODO LIST</h1>
       <div>
         <input />
         <button>ADD</button>
       </div>
+      {/* DRY Don't Repeat Yourself */}
+      {todos.map((todo, index) => (
+        <div key={index}>
+          <input type="checkbox" />
+          <span>{todo}</span>
+          <button>DEL</button>
+        </div>
+      ))}
     </div>
-    </div>
-  )
-};
+  );
+}
 
 export default App;
